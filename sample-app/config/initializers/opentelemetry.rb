63c134d143ec2a06afa1eb03e81e1002837b97fb
Kenshi Muto <kmuto@hatena.ne.jp>
2025-05-28 11:01:33 +0900

    enable a feature to set environment.name

--- sample-app/config/initializers/opentelemetry.rb
+++ sample-app/config/initializers/opentelemetry.rb
@@ -5,10 +5,19 @@ require 'opentelemetry/instrumentation/all'
 if defined?(Rails::Server)
   OpenTelemetry::SDK.configure do |c|
     c.service_name = 'sample-app'
+    h = {}
     if ENV['SERVICE_VERSION']
-      c.resource = OpenTelemetry::SDK::Resources::Resource.create(
-        ENV['USE_SERVICE_VERSION_AS_ENVIRONMENT_NAME'] ? 'deployment.environment.name' :  'service.version' => ENV['SERVICE_VERSION']
-      )
+      if ENV['USE_SERVICE_VERSION_AS_ENVIRONMENT_NAME'] && ENV['USE_SERVICE_VERSION_AS_ENVIRONMENT_NAME'] != 'false'
+        h['deployment.environment.name'] = ENV['SERVICE_VERSION']
+      else
+        h['service.version'] = ENV['SERVICE_VERSION']
+      end
+    end
+    if ENV['ENVIRONMENT_NAME']
+      h['deployment.environment.name'] = ENV['ENVIRONMENT_NAME']
+    end
+    if h
+      c.resource = OpenTelemetry::SDK::Resources::Resource.create(h)
     end
     c.use_all({
       'OpenTelemetry::Instrumentation::Mysql2' => { obfuscation_limit: 10000 },
